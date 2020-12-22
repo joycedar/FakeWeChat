6ddc0d67ff5cf3d936df105c19f3a44f69c16b16
alexju <alexju@tencent.com>
2020-12-22 10:59:10 +0800

    完成测试Badge

--- FakeWeChat/FakeWeChat/Module/Home/FWCHomeViewController.m
+++ FakeWeChat/FakeWeChat/Module/Home/FWCHomeViewController.m
@@ -19,7 +19,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.fwc_tabBarItem.fwc_badgeValue = 200;
+    self.fwc_tabBarItem.fwc_badgeValue = 99;
+
+    QMUIButton *addButton = [QMUIButton.alloc qmui_initWithImage:nil title:@"增加"];
+    QMUIButton *subButton = [QMUIButton.alloc qmui_initWithImage:nil title:@"减少"];
+    [self.view addSubview:addButton];
+    [self.view addSubview:subButton];
+    addButton.centerY = subButton.centerY = self.view.centerY;
+    addButton.centerX = 100;
+    subButton.centerX = 300;
+
+    addButton.height = subButton.height = 100;
+    addButton.width = subButton.width = 100;
+
+    [addButton addTarget:self action:@selector(addBadgeValue) forControlEvents:UIControlEventTouchUpInside];
+    [subButton addTarget:self action:@selector(subBadgeValue) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)addBadgeValue {
+    self.fwc_tabBarItem.fwc_badgeValue++;
+}
+
+- (void)subBadgeValue {
+    self.fwc_tabBarItem.fwc_badgeValue--;
 }
 
 @end
